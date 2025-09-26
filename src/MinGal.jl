4f96d5a9d1f0b0d4ca8f8016c00aeeff97e50f8a
Vitor <vitorlorencone@gmail.com>
2025-09-26 04:33:36 -0300

    Fix MinGal.jl

--- src/MinGal.jl
+++ src/MinGal.jl
@@ -1,6 +1,6 @@
 # TODO
 # -> Implement the multiplicative approach
-# -> More GA Functions
+# -> More GA Functions (Dual)
 # -> Fix Docs and Tests
 # -> Linear Systems Examples
 
@@ -8,7 +8,7 @@ module MinGal
 
 include("Workspace.jl")
 
-export Algebra, grade, grade_projection
+export Algebra, grade, grade_projection, reverse
 export geometric_product, inner_product, outer_product
 export bitmap, scalar, multivector_sum, multivector_sub
 export Multivector, Blade, product_by_scalar, describe
