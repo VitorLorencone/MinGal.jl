c609baa29b95e705a6c2d093ed15c9a7a7b3eff6
Vitor <vitorlorencone@gmail.com>
2025-09-29 06:59:46 -0300

    names change

--- examples/linear_systems.jl
+++ examples/linear_systems.jl
@@ -4,37 +4,37 @@
 using MinGal
 
 """
-    solve_linear_systems(coeff::Array, rhs::Array)::Array
+    solve_linear_systems(A::Array, b::Array)::Array
 
 Function that solves a linear systems of equations using Geometric Algebra
 with the Grassmann method
 
 # Arguments
-- `coeff::Array` : The matrix of coeficients, the left side of the equations.
-- `rhs::Array` : The matrix of values, the right side of the equations.
+- `A::Array` : The matrix of coeficients, the left side of the equations.
+- `b::Array` : The matrix of values, the right side of the equations.
 
 # Return
 An Array with the solutions.
 
 """
-function solve_linear_systems(coeff::Array, rhs::Array)
+function solve_linear_systems(A::Array, b::Array)
     # assume the inputs are always correct
-    D = size(coeff,1)
+    D = size(A,1)
     Algebra(D)
 
     # Calculates the grassmann coeficients such as A1 = a1*e1 + a2*e2 +...+ an*en
-    coeff_grass::GAVector = [sum(Blade(1<<(j-1), coeff[j, i]) for j in 1:D) for i in 1:D]
+    lines::GAVector = [sum(Blade(1<<(j-1), A[j, i]) for j in 1:D) for i in 1:D]
 
     # The same thing but for the right side T = b1*e1 + b2*e2 +...+ bn*en
-    T::GAType = sum(Blade(1<<(j-1), rhs[j]) for j in 1:D)
+    T::GAType = sum(Blade(1<<(j-1), b[j]) for j in 1:D)
 
-    # The outer product of every element in coeff_grass, I = A1^A2^...^An
-    I = invert(reduce(^, coeff_grass; init=Blade(1)))
+    # The outer product of every element in lines, I = A1^A2^...^An
+    I = invert(reduce(^, lines; init=Blade(1)))
 
     # for each variable x in the linear system, compute I^-1 * (A1^A2^...Ax-1^T^Ax+1^...^An)
     ans = zeros(D)
     for i in 1:D
-        substituted_vecs = [j == i ? T : coeff_grass[j] for j in 1:D]
+        substituted_vecs = [j == i ? T : A[j] for j in 1:D]
         partial = reduce(^, substituted_vecs; init=Blade(1))
         ans[i] = (I * partial)[0]
     end
@@ -42,28 +42,28 @@ function solve_linear_systems(coeff::Array, rhs::Array)
     return ans
 end
 
-function solve_linear_systems_homogeneus(coeff::Array, rhs::Array)
+function solve_linear_systems_projective(A::Array, b::Array)
     # assume the inputs are always correct
-    D = size(coeff,1)
+    D = size(A,1)
     Algebra(D, 0, 1)
 
     # Calculates the grassmann coeficients such as A1 = a*e1 + b*e2 +...- B1*e0
-    coeff_grass = Multivector[]
+    lines = Multivector[]
     for i in 1:D
-        mv = sum(Blade(1<<(j), coeff[i,j]) for j in 1:D) - rhs[i]*e0
-        push!(coeff_grass, mv)
+        mv = sum(Blade(1<<(j), A[i,j]) for j in 1:D) - b[i]*e0
+        push!(lines, mv)
     end
 
-    mv = dual(reduce(^, coeff_grass; init=Blade(1)))
-    # mv = reduce(&, dual.(coeff_grass); init=eI) -> Alternative version, but worst
+    mv = dual(reduce(^, lines; init=Blade(1)))
+    # mv = reduce(&, dual.(lines); init=eI) -> Alternative version, but worst
 
     if(mv == 0)
-        error("Compatible and Indeterminate System - Infinite Solutions")
-    elseif(mv[1] == 0)
+        error("Possible and Indeterminate System - Infinite Solutions")
+    elseif(mv[e0] == 0)
         error("Impossible System - No Solution")
     end
 
-    mv = mv/mv[1]
+    mv = mv/mv[e0]
     ans = [mv[1<<k] for k in 1:D]
 end
 
@@ -76,7 +76,7 @@ A1 = [1 2 3;
 B1 = [14, 14, 17]
 
 # Output = [1.0, 2.0, 3.0]
-println(solve_linear_systems_homogeneus(A1, B1))
+println(solve_linear_systems_projective(A1, B1))
 
 # ---------- Example 2 (7x7) ----------
 
@@ -94,7 +94,7 @@ B2 = [32, 25, 20, 19, 21, 20, 21]
 
 # Output = [-2.394202898550725, -1.7768115942028986, 3.7594202898550724, 6.83768115942029, 
 #           1.4492753623188406,  5.144927536231884,  4.602898550724638]
-println(solve_linear_systems_homogeneus(A2, B2))
+println(solve_linear_systems_projective(A2, B2))
 
 # ---------- Example 3 (10x10) ----------
 
@@ -104,4 +104,4 @@ A3 = rand(1:D, D, D)
 B3 = A3 * x_sol
 
 # Output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-println(solve_linear_systems_homogeneus(A3, B3))
+println(solve_linear_systems_projective(A3, B3))
